039ff90f1e1154ce26f6bac3b8e31b29573cab08
Francis Yang <fy50167@gmail.com>
2024-02-02 19:05:50 -0800

    starter canvas

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
-import { useState } from 'react';
+import { useState, useEffect, Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
 
 export default function App() {
-    return <>Hello</>;
+    return (
+        <section className='w-full h-screen relative'>
+            <Canvas
+                shadows
+                camera={{
+                    position: [0, 0, 0],
+                    fov: 70,
+                }}
+            >
+                <color attach='background' args={['lightblue']} />
+            </Canvas>
+        </section>
+    );
 }
